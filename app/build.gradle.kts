209428c18f2ff49f763d9b87651332956e324b04
Luka Nichols <laura.cnichols@outlook.com>
2026-02-15 22:12:22 +0000

    chore: upgraded from SDK 35 to 36 and bumped coreKtx
    Latest coreKtx requires SDK 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.lukafenir.ivy"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.lukafenir.inventoryassistant"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
