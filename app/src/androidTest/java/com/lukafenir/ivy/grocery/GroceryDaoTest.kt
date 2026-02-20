396c5d1f8485afb9088b2d0147cf373bc2082ecb
Luka Nichols <laura.cnichols@outlook.com>
2026-02-20 21:45:48 +0000

    test(#9): tested happy case of all GroceryDao methods

--- app/src/androidTest/java/com/lukafenir/ivy/grocery/GroceryDaoTest.kt
+++ app/src/androidTest/java/com/lukafenir/ivy/grocery/GroceryDaoTest.kt
@@ -36,8 +36,71 @@ class GroceryDaoTest {
         dao.insertItem(GroceryItem(name = "Milk"))
 
         val items = dao.getAllItems().first()
-        assertEquals(1, items.size)
-        assertEquals("Milk", items[0].name)
-        assertEquals(false, items[0].isChecked)
+        assertEquals("Should be one item", 1, items.size)
+        assertEquals("The item's name should be Milk","Milk", items[0].name)
+        assertEquals("The item should be unchecked", false, items[0].isChecked)
+    }
+
+    @Test
+    fun insertItemWithSameName_bothAppearInGetAllItems() = runTest {
+        dao.insertItem(GroceryItem(name = "Milk"))
+        dao.insertItem(GroceryItem(name = "Milk"))
+
+        val items = dao.getAllItems().first()
+        assertEquals("Should be two items", 2, items.size)
+        assertEquals("The first item's name should be Milk","Milk", items[0].name)
+        assertEquals("The second item's name should be Milk","Milk", items[1].name)
+    }
+
+    @Test
+    fun updateItem_changesItemName() = runTest {
+        dao.insertItem(GroceryItem(name = "Milk"))
+
+        val items = dao.getAllItems().first()
+        assertEquals("Item should now be named cheese", "Milk", items[0].name)
+        dao.updateItem(items[0].copy(name = "Cheese"))
+
+        val updatedItems = dao.getAllItems().first()
+        assertEquals("Item should now be named cheese", "Cheese", updatedItems[0].name)
+    }
+
+    @Test
+    fun deleteItem_removesItemFromGetAllItems() = runTest {
+        dao.insertItem(GroceryItem(name = "Milk"))
+        dao.insertItem(GroceryItem(name = "Cheese"))
+
+        val items = dao.getAllItems().first()
+        assertEquals("There should be two items to begin with", 2, items.size)
+        dao.deleteItem(items[0])
+
+        val deletedItems = dao.getAllItems().first()
+        assertEquals("There should be one item", 1, deletedItems.size)
+        assertEquals("The item should be named Cheese", "Cheese", deletedItems[0].name)
+    }
+
+    @Test
+    fun setChecked_changesItemToChecked() = runTest {
+        dao.insertItem(GroceryItem(name = "Milk"))
+
+        val items = dao.getAllItems().first()
+        dao.setChecked(items[0].id, true)
+
+        val updatedItems = dao.getAllItems().first()
+        assertEquals("Item should be checked", true, updatedItems[0].isChecked)
+    }
+
+    @Test
+    fun setCheckedAndUnchecked_changesItemToUnChecked() = runTest {
+        dao.insertItem(GroceryItem(name = "Milk"))
+
+        val items = dao.getAllItems().first()
+        dao.setChecked(items[0].id, true)
+
+        val updatedItems = dao.getAllItems().first()
+        assertEquals("Item should be checked", true, updatedItems[0].isChecked)
+
+        dao.setChecked(items[0].id, false)
+        val finalItems = dao.getAllItems().first()
+        assertEquals("Item should be unchecked", false, finalItems[0].isChecked)
     }
 }
