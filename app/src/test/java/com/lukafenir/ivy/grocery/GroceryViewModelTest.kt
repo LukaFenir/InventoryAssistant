2492d50e6747d3caed1a0a8249deb2e30ee2ef47
Luka Nichols <laura.cnichols@outlook.com>
2026-02-20 21:58:49 +0000

    test(#9): test happy case of all GroceryViewModel methods

--- app/src/test/java/com/lukafenir/ivy/grocery/GroceryViewModelTest.kt
+++ app/src/test/java/com/lukafenir/ivy/grocery/GroceryViewModelTest.kt
@@ -39,9 +39,45 @@ class GroceryViewModelTest {
         viewModel.addItem("Milk")
 
         val items = viewModel.allItems.value
-        assertEquals(1, items.size)
-        assertEquals("Milk", items[0].name)
-        assertEquals(false, items[0].isChecked)
+        assertEquals("Should be one item", 1, items.size)
+        assertEquals("The item's name should be Milk", "Milk", items[0].name)
+        assertEquals("The item should be unchecked", false, items[0].isChecked)
+
+        collectJob.cancel()
+    }
+
+    @Test
+    fun setChecked_updatesItemInRepositoryToChecked() = runTest {
+        // Start a subscriber so WhileSubscribed activates the upstream Flow
+        val collectJob = launch(testDispatcher) { viewModel.allItems.collect {} }
+
+        viewModel.addItem("Milk")
+
+        viewModel.setChecked(viewModel.allItems.value[0].id, true)
+
+        val items = viewModel.allItems.value
+        assertEquals("Should be one item", 1, items.size)
+        assertEquals("The item should be checked", true, items[0].isChecked)
+
+        collectJob.cancel()
+    }
+
+    @Test
+    fun deleteItem_deletesItemFromRepository() = runTest {
+        // Start a subscriber so WhileSubscribed activates the upstream Flow
+        val collectJob = launch(testDispatcher) { viewModel.allItems.collect {} }
+
+        viewModel.addItem("Milk")
+        viewModel.addItem("Cheese")
+        viewModel.addItem("Banana")
+
+        assertEquals("Should be three items", 3, viewModel.allItems.value.size)
+
+        viewModel.deleteItem(viewModel.allItems.value[1])
+        val items = viewModel.allItems.value
+        assertEquals("Should be two items", 2, items.size)
+        assertEquals("The first item's name should be Milk", "Milk", items[0].name)
+        assertEquals("The second item's name should be Banana", "Banana", items[1].name)
 
         collectJob.cancel()
     }
